9a51209dde8546c92768d552d019223c57ea9f09
SDavidT <tumbaco.sergio@gmail.com>
2025-02-11 15:02:41 -0500

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'https://sdavidt.github.io/Portfolio_David_TC',
+  base:"/Portfolio_David_TC/",
 })
